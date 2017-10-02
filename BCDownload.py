a659d8d27f4ee260c913ad8d2d307fca3aaf30d6
jmaxmarno <jmaxmarno@gmail.com>
2017-10-02 12:32:05 -0600

    Error handling for download URL's

--- BCDownload.py
+++ BCDownload.py
@@ -18,13 +18,12 @@ import os
 import requests
 import zipfile
 import datetime
+import sys
+import shutil
 
-# set destination folder to save file
-workspace = r'C:\Users\Max\PycharmProjects\LouisvilleParcels'
-os.chdir(workspace)
 # File urls
 # Boulder County Data in shapefile format
-BC_Parcels = "http://gis.bouldercounty.opendata.arcgis.com/datasets/89ae49d4ddf246388ee5f5e952aa84db_0.zip"
+BC_Parcels = "http://gis.bouldercounty.opendata.arcgis.com/datasets/89ae49d4ddf246388ee5f5e952aa84db_0test.zip"
 Municpalities = "http://gis.bouldercounty.opendata.arcgis.com/datasets/9597d3916aba47e887ca563d5ac15938_0.zip"
 CountyBoundary = "http://gis.bouldercounty.opendata.arcgis.com/datasets/964b8f3b3dbe401bb28d49ac93d29dc4_0.zip"
 # Assessor's Property Data
@@ -38,31 +37,44 @@ Tax_Areas = "http://assessor.boco.solutions/ASR_PublicDataFiles/Tax_Areas.csv"
 Tax_Authorities = "http://assessor.boco.solutions/ASR_PublicDataFiles/Tax_Authorities.csv"
 Values = "http://assessor.boco.solutions/ASR_PublicDataFiles/Values.csv"
 
+file_path = os.path.dirname(os.path.realpath(__file__))
+def create_folder(path):
+    try:
+        if os.path.isdir(path):
+            shutil.rmtree(path)
+            os.makedirs(path)
+        else:
+            os.makedirs(path)
+    except IOError as exception:
+        raise IOError('%s: %s' % (path, exception.strerror))
+    return None
+create_folder(path = (file_path+"\\BC_DATA"))
+os.chdir(file_path+"\\BC_DATA")
 
 ## get today's date for file name
 ##todaystr = str(datetime.date.today())
 ##todaysplit = todaystr.split("-")
 ##todayjoin = "".join(todaysplit)
-
 def bcdnld(urlstr, namestr, fileextstr):
-    print("Downloading with requests: ", urlstr)
     r = requests.get(urlstr)
     if r.status_code == 200:
-        print('Website Exists')
+        print(namestr, 'URL Valid:')
+        print("Downloading with requests: ", urlstr)
         fname = namestr + fileextstr
         # to include today's date in filename substitute line below for line above
         ##    fname = namestr+"_"+todayjoin+fileextstr
         with open(fname, "wb") as code:
             code.write(r.content)
     else:
-        print(namestr, " URL does not exist!")
+        print(namestr, " URL invalid!")
 
 def extractshps(inputzip):
     try:
         zipref = zipfile.ZipFile(inputzip, "r")
         zipref.extractall()
         zipref.close()
-    except
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
 
 
 
@@ -70,22 +82,13 @@ def extractshps(inputzip):
 
 # Download and extract Boulder County Parcel data (shapefile)
 bcdnld(BC_Parcels, "Parcels", ".zip")
-zipref = zipfile.ZipFile("Parcels.zip", "r")
-zipref.extractall()
-zipref.close()
-
+extractshps("Parcels.zip")
 # Download and extract Boulder County Municipalities data (shapefile)
 bcdnld(Municpalities, "Municipalities", ".zip")
-zipref = zipfile.ZipFile("Municipalities.zip", "r")
-zipref.extractall()
-zipref.close()
-
+extractshps("Municipalities.zip")
 # Download and extract Boulder County Parcel data (shapefile)
 bcdnld(CountyBoundary, "CountyBoundary", ".zip")
-zipref = zipfile.ZipFile("CountyBoundary.zip", "r")
-zipref.extractall()
-zipref.close()
-
+extractshps("CountyBoundary.zip")
 # Download Assessor's Property Data
 bcdnld(Values, "Values", ".csv")
 bcdnld(Tax_Authorities, "Tax_Authorities", ".csv")
@@ -97,7 +100,3 @@ bcdnld(Buildings, "Buildings", ".csv")
 bcdnld(Owner_Address, "Owner_Address", ".csv")
 bcdnld(Account_Parcels, "Account_Parcels", ".csv")
 print("Done Downloading")
-
-'''
-tst
-'''
--- README.md
+++
@@ -1,2 +0,0 @@
-# LVParcelUpdate
-Louisville Parcel Update Project
